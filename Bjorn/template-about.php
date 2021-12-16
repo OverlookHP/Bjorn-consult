e7b3e626f865fa121077edf29c7d6275897dc0d0
marlokalle <74901014+marlokalle@users.noreply.github.com>
2021-12-16 09:38:05 +0100

    Update template-about.php
    fixet pinde & gjort responsiv

--- Bjorn/template-about.php
+++ Bjorn/template-about.php
@@ -6,7 +6,7 @@ get_header();
 <body>
 
   <div class="hero-om-os">
-      <img src="<?php bloginfo('template_directory');?>./assets/img/verden.png" alt="BjornConsult" width="100vw" height="200vh">
+      <img src="verden.png" alt="BjornConsult" width="100vw" height="200vh">
   </div>
 
 
@@ -22,7 +22,7 @@ Med en bredere forståelse og indsigt i bæredygtighed på tværs af mærkningso
         </div>
 
     <div class="om-item1right">
-      <img src="<?php bloginfo('template_directory');?>./assets/img/Eirik.png" alt="BjornConsult" width="300" height="300">
+      <img src="verden.png" alt="BjornConsult" width="300" height="300">
     </div>
 
 </div>
@@ -33,25 +33,45 @@ Med en bredere forståelse og indsigt i bæredygtighed på tværs af mærkningso
       <br>
       <p>Med en lang erfaring indenfor udvikling og markeds research hjælper jeg med valg og fokus på både forretningsmodeller, materialer, produkter og processer ud fra markedets nuværende og fremtidige behov og ønsker.</p>
 </div>
-<div class="erfaring-grid">
 
-<div class="zigzag2">
-  <img src="<?php bloginfo('template_directory');?>./assets/img/omos1.png" alt="BjornConsult" width="100" height="100"
- <p>10 Års erfaring som innovationsleder</p>
-</div>
-<div class="zigzag3">
+<div id="main1">
+  <div id="billede1">
+	<div ><img src="<?php bloginfo('template_directory');?>./assets/img/omos1.png" alt="BjornConsult" width="100" height="100"></div>
+  </div>
 
-  <p>17 års erfaring som udvikler for revolutionære projekter <img src="<?php bloginfo('template_directory');?>./assets/img/omos2.png" alt="BjornConsult" width="100" height="100"
-</p>
+  <div id="text1">
+	<div >10 Års erfaring som innovationsleder</div>
+  </div>
 </div>
-<div class="zigzag4">
-  <img src="<?php bloginfo('template_directory');?>./assets/img/omos3.png" alt="BjornConsult" width="100" height="100"
-  <p>24 års erfaring som produktudvikler</p>
+
+
+<div id="main2">
+  <div id="text2">
+	<div >17 års erfaring som udvikler for revolutionære projekter</div>
+  </div>
+  <div id="billede2">
+	<div ><img src="<?php bloginfo('template_directory');?>./assets/img/omos2.png" alt="BjornConsult" width="100" height="100" ></div>
+  </div>
+
 </div>
-<div class="zigzag5">
-  <p>27 års kravsætning af produkter <img src="<?php bloginfo('template_directory');?>./assets/img/omos4.png" alt="BjornConsult" width="100" height="100"
-</p>
+
+<div id="main3">
+  <div id="billede3">
+	<div ><img src="<?php bloginfo('template_directory');?>./assets/img/omos3.png" alt="BjornConsult" width="100" height="100" ></div>
+  </div>
+
+  <div id="text3">
+	<div >24 års erfaring som produktudvikler</div>
+  </div>
 </div>
+
+<div id="main4">
+  <div id="text4">
+	<div >27 års kravsætning af produkter</div>
+  </div>
+  <div id="billede4">
+	<div ><img src="<?php bloginfo('template_directory');?>./assets/img/omos4.png" alt="BjornConsult" width="100" height="100" ></div>
+  </div>
 
 </div>
 </div>
